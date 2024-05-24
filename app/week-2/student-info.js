97f5cb24d1abc1ae8777331f92ef0ca5514b1c4a
RudraYPatel <rudrapatel12241224@gmail.com>
2024-05-24 12:46:27 -0600

    First Commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function StudentInfo() {
+    return(
+        <main>
+            <p>Rudra Patel</p>
+            <Link href="https://github.com/RudraYPatel">Github Link</Link>
+        </main>
+    );
+}
